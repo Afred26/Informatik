1772b610c164cdea6a9380332d7a5f1c0bda687d
Afred26 <schall.fredrik@gmail.com>
2025-03-20 14:20:21 +0100

    Aufgaben bearbeitet

--- 7_JMTW/dhbw/dhbwAufgabe.go
+++ 7_JMTW/dhbw/dhbwAufgabe.go
@@ -5,7 +5,23 @@ package dhbw
 // Vorraussetzung: Löse Rekursiv
 // Foo Liefert das Ergebnis.
 
-func Foo(s string) string {
-	return ""
+func FooS(s string) string {
+	n := len(s)
+	var result string
+	for i := 0; i < n; i++ {
+		result = result + s
+	}
+	return result
 
+}
+
+func FooR(s string) string {
+	return Foo(s, len(s))
+
+}
+func Foo(s string, n int) string {
+	if n == 1 {
+		return s
+	}
+	return s + Foo(s, n-1)
 }
--- 7_JMTW/dhbw/dhbwAufgabe_test.go
+++ 7_JMTW/dhbw/dhbwAufgabe_test.go
@@ -2,15 +2,21 @@ package dhbw
 
 import "fmt"
 
-func ExampleFoo() {
+func ExampleFooS() {
 	t1 := "abc"
 	t2 := "a"
-	t3 := "NNMNNMNNM"
-	fmt.Println(Foo(t1))
-	fmt.Println(Foo(t2))
-	fmt.Println(Foo(t3))
+	t3 := "NNM"
+	fmt.Println(FooS(t1))
+	fmt.Println(FooS(t2))
+	fmt.Println(FooS(t3))
+	fmt.Println(FooR(t1))
+	fmt.Println(FooR(t2))
+	fmt.Println(FooR(t3))
 
 	// Output:
+	// abcabcabc
+	// a
+	// NNMNNMNNM
 	// abcabcabc
 	// a
 	// NNMNNMNNM
